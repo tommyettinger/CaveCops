bdc2c427e6eaa52f8ca2ed9dc2f04c52d91a6b51
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-09-12 22:29:16 -0700

    Interesting effect at least from using YCwCm stuff with the shader's color

--- core/src/main/java/com/github/tommyettinger/CaveCops.java
+++ core/src/main/java/com/github/tommyettinger/CaveCops.java
@@ -35,7 +35,6 @@ import squidpony.squidmath.GreasedRegion;
 import java.util.ArrayList;
 
 import static com.badlogic.gdx.Input.Keys.*;
-import static squidpony.squidgrid.gui.gdx.SColor.FLOAT_WHITE;
 
 /**
  * This is a small, not-overly-simple demo that presents some important features of SquidLib and shows a faster,
@@ -77,7 +76,7 @@ public class CaveCops extends ApplicationAdapter {
     private char[][] decoDungeon, bareDungeon, lineDungeon, prunedDungeon;
     
     public ShaderProgram shader;
-    public Vector3 add, mul;
+//    public Vector3 add, mul;
     private Texture palette;
 
 
@@ -100,7 +99,7 @@ public class CaveCops extends ApplicationAdapter {
     private static final float
             FLOAT_BLOOD = -0x1.564f86p125F,  // same result as SColor.PURE_CRIMSON.toFloatBits()
             FLOAT_LIGHTING = SColor.floatGetHSV(0.17f, 0.12f, 0.8f, 1f),//-0x1.cff1fep126F, // same result as SColor.COSMIC_LATTE.toFloatBits()
-            FLOAT_GRAY = -0x1.7e7e7ep125F; // same result as SColor.CW_GRAY_BLACK.toFloatBits()
+            FLOAT_GRAY = Color.toFloatBits(0.25f, 0.5f, 0.5f, 0.3f);
 
     private InputProcessor input;
 
@@ -132,11 +131,12 @@ public class CaveCops extends ApplicationAdapter {
         // SquidLib has many methods that expect an IRNG instance, and there's several classes to choose from.
         // In this program we'll use GWTRNG, which will behave better on the HTML target than other generators.
         rng = new GWTRNG(1337);
-        shader = new ShaderProgram(ShaderUtils.vertexShader, ShaderUtils.fragmentShaderWarmMildLimited);
+        shader = new ShaderProgram(ShaderUtils.vertexShader, ShaderUtils.fragmentShader);
+//        shader = new ShaderProgram(ShaderUtils.vertexShader, ShaderUtils.fragmentShaderWarmMildLimited);
         if (!shader.isCompiled()) throw new GdxRuntimeException("Couldn't compile shader: " + shader.getLog());
         batch = new FilterBatch(8000, shader);
-        add = new Vector3(0, 0, 0);
-        mul = new Vector3(1, 1, 1);
+//        add = new Vector3(0, 0, 0);
+//        mul = new Vector3(1, 1, 1);
 
         mainViewport = new PixelPerfectViewport(Scaling.fill, gridWidth * cellWidth, gridHeight * cellHeight);
         mainViewport.setScreenBounds(0, 0, gridWidth * cellWidth, gridHeight * cellHeight);
@@ -147,8 +147,8 @@ public class CaveCops extends ApplicationAdapter {
         font = new BitmapFont(Gdx.files.internal("font.fnt"), atlas.findRegion("font"));
         font.getData().scale(2f);
         
-//        palette = new Texture("DawnSmash256_GLSL.png");
-        palette = new Texture("GBGreen16_GLSL.png");
+        palette = new Texture("DawnSmash256_GLSL.png");
+//        palette = new Texture("GBGreen16_GLSL.png");
         
         charMapping = new IntMap<>(64);
         solid = atlas.findRegion("day tile floor c");
@@ -301,12 +301,8 @@ public class CaveCops extends ApplicationAdapter {
         // GreasedRegion that contains the cells just past the edge of the player's FOV area.
         playerToCursor.partialScan(13, blockage);
 
-        //The next three lines set the background color for anything we don't draw on, but also create 2D arrays of the
-        //same size as decoDungeon that store the colors for the foregrounds and backgrounds of each cell as packed
-        //floats (a format SparseLayers can use throughout its API), using the colors for the cell with the same x and
-        //y. By changing an item in SColor.LIMITED_PALETTE, we also change the color assigned by MapUtility to floors.
-        bgColor = new Color(0x132C2DFF);
-//        bgColor = SColor.DB_INK;
+//        bgColor = new Color(0x132C2DFF); // for GBGreen16
+        bgColor = new Color(0x140C1CFF);   // for DawnSmash256
 //        for (int x = 0; x < bigWidth; x++) {
 //            for (int y = 0; y < bigHeight; y++) {
 //                colors[x][y] = f(colors[x][y]);
@@ -469,15 +465,18 @@ public class CaveCops extends ApplicationAdapter {
             for (int j = 0; j < bigHeight; j++) {
                 if(visible[i][j] > 0.0) {
                     pos.set(i * cellWidth, j * cellHeight, 0f);
-                    batch.setPackedColor(toCursor.contains(Coord.get(i, j))
-                            ? FLOAT_WHITE
-                            : SColor.lerpFloatColors(FLOAT_GRAY, FLOAT_LIGHTING, (float)visible[i][j] * 0.75f + 0.25f));
+//                    batch.setPackedColor(toCursor.contains(Coord.get(i, j))
+//                            ? FLOAT_WHITE
+//                            : SColor.lerpFloatColors(FLOAT_GRAY, FLOAT_LIGHTING, (float)visible[i][j] * 0.75f + 0.25f));
+                    batch.setColor(toCursor.contains(Coord.get(i, j))
+                                    ? 0.9f
+                                    : (float)visible[i][j] * 0.5f + 0.2f,
+                            0.5f, 0.5f, 0.4f);
                     //batch.draw(solid, pos.x, pos.y);                     
 //                    batch.setPackedColor(SColor.lerpFloatColors(colors[i][j], FLOAT_LIGHTING, (float)visible[i][j] * 0.75f + 0.25f));
                     batch.draw(charMapping.get(prunedDungeon[i][j], solid), pos.x, pos.y);
                 } else if(seen.contains(i, j)) {
                     pos.set(i * cellWidth, j * cellHeight, 0f);
-                    batch.setPackedColor(FLOAT_GRAY);
                     //batch.draw(solid, pos.x, pos.y);
 //                    if ((monster = monsters.get(Coord.get(i, j))) != null)
 //                        monster.setAlpha(0f);
@@ -508,8 +507,8 @@ public class CaveCops extends ApplicationAdapter {
         palette.bind();
         batch.begin();
         shader.setUniformi("u_palette", 1);
-        shader.setUniformf("u_mul", mul);
-        shader.setUniformf("u_add", add);
+//        shader.setUniformf("u_mul", mul);
+//        shader.setUniformf("u_add", add);
         Gdx.gl.glActiveTexture(GL20.GL_TEXTURE0);
         putMap();
         // if the user clicked, we have a list of moves to perform.
--- core/src/main/java/com/github/tommyettinger/ShaderUtils.java
+++ core/src/main/java/com/github/tommyettinger/ShaderUtils.java
@@ -23,6 +23,30 @@ public class ShaderUtils {
             + "   v_texCoords = " + ShaderProgram.TEXCOORD_ATTRIBUTE + "0;\n"
             + "   gl_Position =  u_projTrans * " + ShaderProgram.POSITION_ATTRIBUTE + ";\n"
             + "}\n";
+    public static final String fragmentShader =
+            "varying vec2 v_texCoords;\n" +
+                    "varying vec4 v_color;\n" +
+                    "uniform sampler2D u_texture;\n" +
+                    "uniform sampler2D u_palette;\n" +
+                    "const float b_adj = 31.0 / 32.0;\n" +
+                    "const float rb_adj = 32.0 / 1023.0;\n" +
+                    "const vec3 bright = vec3(0.375, 0.5, 0.125);\n" +
+                    "void main()\n" +
+                    "{\n" +
+                    "   vec4 tgt = texture2D( u_texture, v_texCoords );\n" +
+                    "   vec4 used = texture2D(u_palette, vec2((tgt.b * b_adj + floor(tgt.r * 31.999)) * rb_adj, 1.0 - tgt.g));\n" +
+//                    "   float len = dot(tgt.rgb, bright * 0.0625) + 1.0;\n" +
+//                    "   float adj = fract(52.9829189 * fract(dot(vec4(0.06711056, 0.00583715, 0.7548776662466927, 0.5698402909980532), gl_FragCoord.xyyx))) - 0.5;\n" +
+//                    "   float len = fract(dot(tgt.rgb, bright) * dot(sin(gl_FragCoord.xy * 5.6789), vec2(14.743036261279236, 13.580412143837574)));\n" +
+//                    "   float adj = len * 0.4 - 0.2;\n" +
+                    "   float len = dot(tgt.rgb, bright * 0.0625) + 0.5;\n" +
+                    "   float adj = (fract(52.9829189 * fract(dot(vec2(0.06711056, 0.00583715), sin(gl_FragCoord.xy + len) + cos(gl_FragCoord.yx)))) - 0.5) * len;\n" +
+                    "   tgt.rgb = 2.0 * vec3(v_color.r * dot(tgt.rgb, bright), (v_color.g - 0.5) + v_color.a * (tgt.r - tgt.b), (v_color.b - 0.5) + v_color.a * (tgt.g - tgt.b));\n" +
+                    "   tgt.rgb = clamp(vec3(dot(tgt.rgb, vec3(1.0, 0.625, -0.5)), dot(tgt.rgb, vec3(1.0, -0.375, 0.5)), dot(tgt.rgb, vec3(1.0, -0.375, -0.5))), 0.0, 1.0);\n" +
+                    "   tgt.rgb = clamp(tgt.rgb + (tgt.rgb - used.rgb) * adj, 0.0, 1.0);\n" +
+                    "   gl_FragColor.rgb = texture2D(u_palette, vec2((tgt.b * b_adj + floor(tgt.r * 31.999)) * rb_adj, 1.0 - tgt.g)).rgb;\n" +
+                    "   gl_FragColor.a = tgt.a;\n" +
+                    "}";
     public static final String fragmentShaderWarmMildLimited =
             "varying vec2 v_texCoords;\n" +
                     "varying vec4 v_color;\n" +
@@ -41,12 +65,7 @@ public class ShaderUtils {
                     "   float adj = fract(52.9829189 * fract(dot(vec2(0.06711056, 0.00583715), gl_FragCoord.xy + len))) * len - len * 0.5;\n" +
                     "   tgt.rgb = clamp(tgt.rgb + (tgt.rgb - used.rgb) * adj, 0.0, 1.0);\n" +
                     "   tgt.rgb = u_add + u_mul * vec3(dot(tgt.rgb, bright), tgt.r - tgt.b, tgt.g - tgt.b);\n" +
-                    //// this is an alternate way but it messes up the colors on the blue to yellow axis
-//                    "   tgt.rgb = clamp(vec3(dot(tgt.rgb, vec3(1.0, 0.625, 0.0)), dot(tgt.rgb, vec3(1.0, 0.0, 0.5)), dot(tgt.rgb, vec3(1.0, -0.5, -0.375))), 0.0, 1.0);\n" +
-                    //// this is the documented "correct" way, and it seems to cover the full gamut
                     "   tgt.rgb = clamp(vec3(dot(tgt.rgb, vec3(1.0, 0.625, -0.5)), dot(tgt.rgb, vec3(1.0, -0.375, 0.5)), dot(tgt.rgb, vec3(1.0, -0.375, -0.5))), 0.0, 1.0);\n" +
-                    //// this is an alternative that seems to cover more colors, at least at medium luma levels, but has serious issues with blue and yellow tinting
-//                    "   tgt.rgb = clamp(vec3(dot(tgt.rgb, vec3(1.0, 0.5, 0.0)), dot(tgt.rgb, vec3(1.0, 0.0, 0.5)), dot(tgt.rgb, vec3(1.0, -0.25, -0.25))), 0.0, 1.0);\n" +
                     "   gl_FragColor.rgb = v_color.rgb * texture2D(u_palette, vec2((tgt.b * b_adj + floor(tgt.r * 31.999)) * rb_adj, 1.0 - tgt.g)).rgb;\n" +
                     "   gl_FragColor.a = v_color.a * tgt.a;\n" +
                     "}";
