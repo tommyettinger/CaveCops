7764ac6bc73dc19d398867b6f60f6e5a36040fa3
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-08-26 21:09:28 -0700

    Update Gradle and Gretty!

--- desktop/src/main/java/com/github/tommyettinger/desktop/ﾠ.java
+++ desktop/src/main/java/com/github/tommyettinger/desktop/ﾠ.java
@@ -1,11 +1,14 @@
 package com.github.tommyettinger.desktop;
 
+/**
+ * What's, that, invisible class? Lets you call while (); and it will pass.
+ */
 public class ﾠ{
     /**
      * If you static-import this function, you can call
      * {@code whileﾠ(true);} and it won't infinite-loop.
+     *
      * @param condition ignored
      */
-    public static void whileﾠ(boolean condition) {
-    }
+    public static void whileﾠ(boolean condition){}
 }
